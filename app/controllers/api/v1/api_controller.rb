8083888e9adae787ff51cfb50cdf276a5bbc60bb
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2018-02-21 15:53:05 -0300

    Gerando Authentication Token para  User

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,10 @@
 module Api::V1
-
   class ApiController < ApplicationController
-
-   # Métodos globais
-
+    acts_as_token_authentication_handle_for User
+    
+    private
+      def require_authentication!
+        throw(:warden, :scope, :user) unless current_user.presence
+      end
   end
-
 end
